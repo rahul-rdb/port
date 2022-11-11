02754ba69287edaa869dcdaecbd69a287673bd0c
rahul-rdb <rahulbobade26@gmail.com>
2022-11-11 14:46:37 +0530

    added tools

--- src/components/ToolsPage/index.js
+++ src/components/ToolsPage/index.js
@@ -6,7 +6,9 @@ function ToolsPage() {
     <div className="tools-bg">
       <div className="tools-wrapper flex flex-col max-width">
         <div className="tools">
-          <p>Languages: HTML, CSS, JavaScript (ReactJs)</p>
+          <p>Languages: HTML, CSS, JavaScript</p>
+          <p>Frameworks: ReactJs</p>
+          <p>UI library: Material UI</p>
           <p>Tools: Git, GitHub, VS code</p>
           <p>Other: AutoCAD, Solidworks(basics)</p>
           <p>Language Spoken: English, Hindi, Marathi</p>
